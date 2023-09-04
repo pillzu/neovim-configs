9fff6838cfe22dca6d15186e562d74956eb6482e
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-09-04 09:29:40 -0400

    nvchad config for tree

--- lua/core/plugins/essentials.lua
+++ lua/core/plugins/essentials.lua
@@ -234,17 +234,90 @@ return {
       end
 
       require("nvim-tree").setup({
-        sort_by = "case_sensitive",
+        filters = {
+          dotfiles = false,
+          exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
+        },
+        disable_netrw = true,
+        hijack_netrw = true,
+        hijack_cursor = true,
+        hijack_unnamed_buffer_when_opening = false,
+        sync_root_with_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = false,
+        },
         view = {
-
+          adaptive_size = false,
+          side = "left",
           width = 30,
+          preserve_window_proportions = true,
+        },
+        git = {
+          enable = false,
+          ignore = true,
+        },
+        filesystem_watchers = {
+          enable = true,
+        },
+        actions = {
+          open_file = {
+            resize_window = true,
+          },
         },
         renderer = {
-          group_empty = true,
+          root_folder_label = false,
+          highlight_git = false,
+          highlight_opened_files = "none",
+
+          indent_markers = {
+            enable = false,
+          },
+
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = false,
+            },
+
+            glyphs = {
+              default = "󰈚",
+              symlink = "",
+              folder = {
+                default = "",
+                empty = "",
+                empty_open = "",
+                open = "",
+                symlink = "",
+                symlink_open = "",
+                arrow_open = "",
+                arrow_closed = "",
+              },
+              git = {
+                unstaged = "✗",
+                staged = "✓",
+                unmerged = "",
+                renamed = "➜",
+                untracked = "★",
+                deleted = "",
+                ignored = "◌",
+              },
+            },
+          },
         },
-        filters = {
-          dotfiles = true,
-        },
+        sort_by = "case_sensitive",
+        -- view = {
+        --
+        --   width = 30,
+        -- },
+        -- renderer = {
+        --   group_empty = true,
+        -- },
+        -- filters = {
+        --   dotfiles = true,
+        -- },
         on_attach = my_on_attach
       })
     end
