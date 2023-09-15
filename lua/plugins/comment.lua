f5c3478026cc49a5e3cec5fbd7f9434b8eb18082
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2023-09-15 10:05:07 -0400

    fix: comment shortcuts

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,14 +1,21 @@
 return {
   {
-    -- comment using <leader>/
     'numToStr/Comment.nvim',
-    opts = {
-      toggler = {
-        line = '<leader>/',
-      },
-      opleader = {
-        line = '<leader>/',
-      }
-    }
+    keys = {
+      { "gcc", mode = "n",          desc = "Comment toggle current line" },
+      { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n",          desc = "Comment toggle current block" },
+      { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+    },
+    init = function()
+      vim.keymap.set('n', "<leader>/", require('Comment.api').toggle.linewise.current, { desc = "Toggle Comment" })
+      vim.keymap.set('v', "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+        { desc = "Toggle Comment" })
+    end,
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end
   }
 }
