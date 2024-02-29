7ac82455841de374203ab0632f59966250fc306c
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2024-02-29 10:07:00 -0500

    fix: tmu<->nvim navigation fixed

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -53,9 +53,3 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
--- Essential movement keys
-vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -5,7 +5,22 @@ return {
   -- 'dstein64/vim-startuptime', -- NOTE: Only when testing
 
   -- tmux <-> vim navigation
-  'christoomey/vim-tmux-navigator',
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require 'nvim-tmux-navigation'.setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        }
+      }
+    end
+  },
 
   -- maximize and restore current window
   'szw/vim-maximizer',
