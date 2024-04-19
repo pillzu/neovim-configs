119c6139af721cf0874bbbc9b7ff088ade120b89
Piyush Upadhyay <piyushupadhyay2566@gmail.com>
2024-04-19 18:29:26 -0400

    disable lsp progress

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,10 @@ return {
     event = "VeryLazy",
     opts = {
       lsp = {
+        -- disable bottom half progress
+        progress = {
+          enabled = false
+        },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
